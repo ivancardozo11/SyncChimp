b1102889d6f775568d7b6c0f080138e57ca771cd
Ivan Cardozo <ivancardozo11@hotmail.com>
2023-02-25 10:42:56 +0100

    add packages dotenv and mailchimp marketing package, remove unnecesary package and create mailchimp configuration module

---
+++ mailchimp.config.ts
@@ -0,0 +1,12 @@
+import mailchimp from "@mailchimp/mailchimp_marketing";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+ function configureMailchimp() {
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: process.env.MAILCHIMP_SERVER_PREFIX,
+  });
+}
+export default configureMailchimp;
